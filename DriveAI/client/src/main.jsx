8e0cac027141c06ab134f0efbb02bba05352fe8d
Abhi123-s <abhi313102@gmail.com>
2026-04-18 11:27:20 +0530

    second commit

--- DriveAI/client/src/main.jsx
+++ DriveAI/client/src/main.jsx
@@ -6,8 +6,12 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import axios from "axios";
 import "./index.css"; // Global styles + Tailwind
 import App from "./App.jsx";
+
+// Use the backend URL from environment variables if available
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL || "";
 
 // Mount the app into the <div id="root"> in index.html
 createRoot(document.getElementById("root")).render(
